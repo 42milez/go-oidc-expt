6f9d90ca55eb905db6528b9349d7110c4c401f0d
Akihiro TAKASE <42milez@gmail.com>
2023-08-11 21:44:42 +0900

    Refactor

--- app/idp/model/authentication.go
+++ app/idp/model/authentication.go
@@ -1,3 +1,3 @@
 package model
 
-type Authenticate struct {}
+type Authenticate struct{}
--- app/idp/model/http.go
+++ app/idp/model/http.go
@@ -1,5 +1,5 @@
 package model
 
-type BadRequest struct {}
+type BadRequest struct{}
 
-type InternalServerError struct {}
+type InternalServerError struct{}
