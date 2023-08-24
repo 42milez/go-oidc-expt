cc3ed49e91864f40a25f09b2b138ce4b26a71d08
Akihiro TAKASE <42milez@gmail.com>
2023-08-24 13:20:30 +0900

    Add script to make random string

---
+++ script/randomkey/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/42milez/go-oidc-server/pkg/xutil"
+	"log"
+)
+
+var (
+	lenFlag = flag.Int("len", 0, "the length of random string")
+)
+
+func main() {
+	flag.Parse()
+
+	if *lenFlag == 0 {
+		log.Fatal("length must be greater than 0")
+	}
+
+	ret, err := xutil.MakeCryptoRandomString(*lenFlag)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(ret)
+}
