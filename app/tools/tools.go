5b82a96e28794648860f10b891c9effd634b6ce8
Akihiro TAKASE <42milez@gmail.com>
2023-10-25 19:13:14 +0900

    Refactor

--- app/tools/tools.go
+++ app/tools/tools.go
@@ -3,6 +3,7 @@
 package tools
 
 import (
+	_ "github.com/Masterminds/sprig"
 	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
 	_ "github.com/golang/mock/gomock"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
